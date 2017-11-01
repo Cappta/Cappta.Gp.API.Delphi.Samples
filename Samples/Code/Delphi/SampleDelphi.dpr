28eef286edea3933fa440a522976484e91ea2998
Silvio Zanelato <silvio.zanelato@cappta.com.br>
2017-11-01 15:31:38 -0200

    Added Delphi integration samples

---
+++ Samples/Code/Delphi/SampleDelphi.dpr
@@ -0,0 +1,14 @@
+program SampleDelphi;
+
+uses
+  Vcl.Forms,
+  sample in 'sample.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
